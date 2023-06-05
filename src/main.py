f969ad75331bb2afdc9cfc15c7a645a9d5d8de7d
Igoreha <trueoss1992@gamil.com>
2023-06-05 13:58:24 +0700

    corrt vision

--- src/main.py
+++ src/main.py
@@ -4,6 +4,53 @@ from functools import wraps
 import multiprocessing
 
 
+def compare_two_combs(h1, h2):
+    comb_one = get_comb_details(h1)
+    comb_two = get_comb_details(h2)
+    rank1 = calc_rank_comb(comb_one)
+    rank2 = calc_rank_comb(comb_two)
+    if rank1 == rank2:
+        if comb_one < comb_two:
+            return "Win"
+        elif comb_one > comb_two:
+            return "Lose"
+        else:
+            return "Draw"
+    return rank1 < rank2 if "Win" else "Lose"
+
+
+def get_comb_details(comb: str) -> tuple:
+    copy_comb = comb.split(' ')
+    detail_comb = ''
+    suits_comb_str = ''
+    for elem in copy_comb:
+        detail_comb += deck[elem]
+        suits_comb_str += elem[1]
+    return (''.join(sorted(detail_comb)), is_flash_nahd(suits_comb_str))
+
+
+def calc_rank_comb(comb: tuple):
+    high_straight = is_high_straight_comb(comb[0])
+    flash = comb[1]
+    straight = is_straight_comb(comb[0])
+    caret = calc_duplication(comb[0])['caret'] > 0
+    trips = calc_duplication(comb[0])['trips'] > 0
+    two_pair = calc_duplication(comb[0])['pair'] > 1
+    one_pair = calc_duplication(comb[0])['pair'] > 0
+    rank = \
+        (flash and high_straight and 1) or \
+        (flash and straight and 2) or \
+        (caret and 3) or \
+        (trips and one_pair and 4) or \
+        (flash and 5) or \
+        (straight and 6) or \
+        (trips and 7) or \
+        (two_pair and 8) or \
+        (one_pair and 9) or \
+        (10)
+    return rank
+
+
 def timeit(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -13,6 +60,28 @@ def timeit(func):
         print(f'{func.__name__} took {end - start:.6f} seconds to complete')
         return result
     return wrapper
+
+
+@timeit
+def calc_options():
+    for i in range(50_000):
+        compare_two_combs('2c 5c 3c 4c 5h 7c Ad', 'Ac Ac 3c 4c 5h 7c 6d')
+
+
+def tmp_func():
+    for i in range(50_000):
+        compare_two_combs('2c 5c 3c 4c 5h 7c Ad', 'Ac Ac 3c 4c 5h 7c 6d')
+
+
+@timeit
+def paralel_calc_options():
+    processes = []
+    for i in range(8):
+        proc = multiprocessing.Process(target=tmp_func)
+        processes.append(proc)
+        proc.start()
+    for elem in processes:
+        elem.join()
 
 
 def is_flash_nahd(suits_comb_str: str):
@@ -47,78 +116,9 @@ def calc_duplication(comb: str):
     return result
 
 
-def getcombDetails(comb: str) -> tuple:
-    copy_comb = comb.split(' ')
-    detail_comb = ''
-    suits_comb_str = ''
-    for elem in copy_comb:
-        detail_comb += deck[elem]
-        suits_comb_str += elem[1]
-    return (''.join(sorted(detail_comb)), is_flash_nahd(suits_comb_str))
-
-
 def is_high_straight_comb(comb: str):
     return is_straight_comb(comb) and 'A' in comb and 'K' in comb \
         and 'Q' in comb
-
-
-def calc_rank_comb(comb: tuple):
-    high_straight = is_high_straight_comb(comb[0])
-    flash = comb[1]
-    straight = is_straight_comb(comb[0])
-    caret = calc_duplication(comb[0])['caret'] > 0
-    trips = calc_duplication(comb[0])['trips'] > 0
-    two_pair = calc_duplication(comb[0])['pair'] > 1
-    one_pair = calc_duplication(comb[0])['pair'] > 0
-    rank = \
-        (flash and high_straight and 1) or \
-        (flash and straight and 2) or \
-        (caret and 3) or \
-        (trips and one_pair and 4) or \
-        (flash and 5) or \
-        (straight and 6) or \
-        (trips and 7) or \
-        (two_pair and 8) or \
-        (one_pair and 9) or \
-        (10)
-    return rank
-
-
-def comparecombs(h1, h2):
-    comb_one = getcombDetails(h1)
-    comb_two = getcombDetails(h2)
-    rank1 = calc_rank_comb(comb_one)
-    rank2 = calc_rank_comb(comb_two)
-    if rank1 == rank2:
-        if comb_one < comb_two:
-            return "Win"
-        elif comb_one > comb_two:
-            return "Lose"
-        else:
-            return "Draw"
-    return rank1 < rank2 if "Win" else "Lose"
-
-
-@timeit
-def calc_options():
-    for i in range(100_000):
-        comparecombs('2c 5c 3c 4c 5h 7c Ad', 'Ac Ac 3c 4c 5h 7c 6d')
-
-
-def tmp_func():
-    for i in range(100_000):
-        comparecombs('2c 5c 3c 4c 5h 7c Ad', 'Ac Ac 3c 4c 5h 7c 6d')
-
-
-@timeit
-def paralel_calc_options():
-    processes = []
-    for i in range(8):
-        proc = multiprocessing.Process(target=tmp_func)
-        processes.append(proc)
-        proc.start()
-    for elem in processes:
-        elem.join()
 
 
 paralel_calc_options()
